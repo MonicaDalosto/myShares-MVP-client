d7c4608285b2b1c361d5daf7f629de894a400272
MonicaDalosto <monica.kerber@gmail.com>
2022-07-02 22:41:31 +0200

    feat[login]: Updated the login page and change the colors

--- src/styled/Button.js
+++ src/styled/Button.js
@@ -1,15 +1,20 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 export const Button = styled.button`
-  background: ${props => props.primary ? "#B22727" : "white"};
-  color: ${props => props.primary ? "white" : "#B22727"};
+  background: ${props => (props.primary ? '#000050' : 'white')};
+  color: ${props => (props.primary ? 'white' : '#000050')};
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid #B22727;
+  border: 2px solid #000050;
   border-radius: 3px;
-  
-  &:hover{
-    border: 2px solid #1E3163
+
+  &:hover {
+    background-color: #000050;
+    color: white;
+    // border: 3px solid #000050;
   }
 `;
+
+// &:hover {
+// }
