103dedc364e185432fdc1716cb44a460b1c0008e
MonicaDalosto <monica.kerber@gmail.com>
2022-07-22 13:40:24 +0200

    Add navigate to the CompanyDashboard

--- src/pages/CompanyDashboard.jsx
+++ src/pages/CompanyDashboard.jsx
@@ -2,17 +2,24 @@ import { Title } from '../styled';
 import { Container } from '../styled';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { selectToken } from '../store/user/selectors';
 import { getAllEmployeesContractsSummary } from '../store/contracts/thunks';
 import { selectAllEmployeeContractsSummary } from '../store/contracts/selectors';
 import { CompanyTableShares } from '../components';
 
 const CompanyDashboard = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const token = useSelector(selectToken);
   const allEmployeeContracts = useSelector(selectAllEmployeeContractsSummary);
 
   useEffect(() => {
+    if (token === null) {
+      navigate('/login');
+    }
     dispatch(getAllEmployeesContractsSummary());
-  }, [dispatch]);
+  }, [dispatch, token, navigate]);
 
   if (!allEmployeeContracts) {
     return (
