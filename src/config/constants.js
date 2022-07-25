6e5efacd0ce52a8c5f10fbf28c29ae49badd51f6
MonicaDalosto <monica.kerber@gmail.com>
2022-07-25 16:16:07 +0200

    Create multiple environments

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,2 @@
-export const apiUrl =
-  process.env.API_URL || 'https://mysharescompany-codaisseur.herokuapp.com';
+export const apiUrl = process.env.REACT_APP_LINK_API;
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
